06b2be5d7787256650b340b8a66b452a53982e31
Ana12Tec <47459796+Ana12Tec@users.noreply.github.com>
2022-11-11 14:06:35 -0600

    Commit de prueba

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
 import { AuthService } from './../services/auth.service';
 
 @Injectable({
